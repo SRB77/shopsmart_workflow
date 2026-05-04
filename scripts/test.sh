f737e25a14039ab0fcce34cfafcd3c541eb9ab13
SRB77 <soumyach144@gmail.com>
2026-05-04 15:56:07 +0530

    remove old pre-written script and adding scripts as per my learning

---
+++ scripts/test.sh
@@ -0,0 +1,8 @@
+# !/bin/bash 
+# this is shebang which tells the system which interpreter or shell to use 
+echo "Starting ..."
+mkdir something
+echo "Done" 
+
+#First make this script executable using "chmod +x test.sh"
+# Then run the file using ./test.sh tried and tested
