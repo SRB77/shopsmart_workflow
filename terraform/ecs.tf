c39c9f780020ae3a2514ac957598bb64e44ab083
SRB77 <soumyach144@gmail.com>
2026-05-05 13:44:28 +0530

    fix iam role issue in ecs.tf

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -3,25 +3,9 @@ resource "aws_ecs_cluster" "cluster" {
   name = "shopsmart-cluster"
 }
 
-# IAM Role
-resource "aws_iam_role" "ecs_task_execution_role" {
+# ✅ Use EXISTING IAM Role (instead of creating)
+data "aws_iam_role" "ecs_task_execution_role" {
   name = "ecsTaskExecutionRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "ecs-tasks.amazonaws.com"
-      }
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
-  role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 # Task Definition
@@ -31,7 +15,7 @@ resource "aws_ecs_task_definition" "task" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  execution_role_arn       = data.aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([
     {
